d956a461dafcf7a3dafc374f9c363ffee329d250
Fabian Mild <fabian.mild@gmx.at>
2022-06-26 12:31:34 +0200

    Hab mal AES und RSA hinzugefügt und den Content im Konstruktor des Packtes verschlüsselt

--- src/main/java/spg/server/PacketByteBuffer.java
+++ src/main/java/spg/server/PacketByteBuffer.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class PacketByteBuffer implements Serializable {
 	public static class Builder {
@@ -38,9 +39,27 @@ public class PacketByteBuffer implements Serializable {
 	}
 
 	private final byte[] content;
+	private HashMap<String, byte[]> userKeys = new HashMap<String,byte[]>();	//befüllt mit den Namen der User(müssen daher vom server eindeutig verifiziert werden: sonst keine verbindung!   ----    und die dazu verschlüsselten versionen des threadkeys)
 
-	public PacketByteBuffer(byte[] content) {
-		this.content = content;
+	public PacketByteBuffer(byte[] content) {//solten auch die reciever im Konstruktor sein
+
+		byte[] x= content;
+
+		AES aes= new AES();
+		String key = AES.genKey();
+		x= aes.encrypt(Arrays.toString(content),key).getBytes();	//tatsächliche Verschlüsselung des Contents
+
+		//Hashmap befüllen
+
+		/*
+		for (ClientMain client:receivers) { //hashmap befüllen
+			byte[] encodedThreadKey = RSA.encode(key, client.publicKey, client.N);
+			userKeys.put(client.getUsername(), encodedThreadKey);
+		}
+		*/
+
+
+		this.content = x;
 	}
 
 	public byte[] getContent() {
---
+++ src/main/java/spg/server/RSA.java
@@ -0,0 +1,60 @@
+package spg.server;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+class RSA
+{
+/*
+    p   //primzahl 1
+    q   //primzahl 2
+    N   //Teil beider Keys
+    phi //eulersche phi-funktion
+    e   //Teil des public key
+    d   //Teil des private key
+*/
+
+    private static final int bitlength = 4096;
+
+    public static BigInteger[] genKeyPair_plusN(){  //first ist private, then is public, then is n
+        SecureRandom r= new SecureRandom();
+        BigInteger p= BigInteger.probablePrime(bitlength,r);
+        BigInteger q= BigInteger.probablePrime(bitlength,r);
+        BigInteger N= p.multiply(q);
+        BigInteger phi= p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));   //Da p und q primzahlen sind, muss man nur 1 wegrechnen (Mathehenker)
+        BigInteger e= BigInteger.probablePrime(bitlength/2,r);
+
+        while(phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0){
+            e= e.add(BigInteger.ONE);
+        }
+        BigInteger d= e.modInverse(phi);
+
+
+        //nun die generierten Schlüssel übergeben
+        BigInteger[] x= new BigInteger[3];
+        x[0]=d;
+        x[1]=e;
+        x[2]=N;
+
+        return x;
+    }
+
+    public static byte[] encrypt(String msg, BigInteger key, BigInteger N){  //key==e
+        return (new BigInteger(msg.getBytes())).modPow(key,N).toByteArray();
+    }
+    //die Decode-Methode ist im Client, da man da den private-key braucht, den eben nur der Client hat =)
+
+
+    /*public static Packet bytesToObject(byte[] encoded){
+        try(ByteArrayInputStream is= new ByteArrayInputStream(encoded); ObjectInputStream oi= new ObjectInputStream(is)){
+            return (Packet)oi.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }*/
+
+}
